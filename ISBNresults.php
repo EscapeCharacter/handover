6a7237004f057629d7852cb0ede6adab74b217a7
Sagiv Lapkin <lapkins@MacBook-Pro.local>
2018-09-13 14:24:50 -0400

    First commit

---
+++ ISBNresults.php
@@ -0,0 +1,83 @@
+<?php
+
+function return_data(){
+	$baseURLbegin = "http://10.3.104.26:8983/solr/live-gt/select?indent=on&q=ISBN:%22";
+	$baseURLend	  = "%22&wt=json";
+	$userISBN = preg_replace("/-/", "", $_POST['isbn_input']);
+	$fullURL = $baseURLbegin . $userISBN . $baseURLend;
+	$JSONraw = file_get_contents($fullURL);
+	$JSON = json_decode($JSONraw);
+	print "<pre>";
+	print_r($JSON);
+	print "</pre>";
+}
+
+if (isset($_POST['submit'])) {
+	return_data();
+}
+
+function handle_upload(){
+	
+	$target_file = basename($_FILES["fileToUpload"]["name"]);
+	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+	if($imageFileType == "xls" || $imageFileType == "xlsx" || $imageFileType == "csv"){
+	    $target_dir = "excel/";
+	} elseif($imageFileType == "doc" || $imageFileType == "docx"){
+	    $target_dir = "word/";
+	} else{
+		$target_dir = "uploads/";
+	}
+
+	$target_file = $target_dir . $target_file;
+
+	$errorMsg = null;
+		
+	if (file_exists($target_file)) {
+		$errorMsg = "Sorry, file already exists.";
+	}
+
+	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        $errorMsg = "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        $errorMsg = "Sorry, there was an error uploading your file.";
+    }
+	
+	echo $errorMsg;
+}
+
+if(isset($_POST['submit2'])){
+	handle_upload();
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ISBN Info</title>
+</head>
+
+<body>
+
+<form action="index.php">
+	<button>Back Home</button>
+</form>
+
+<div>
+	<form enctype="multipart/form-data" name="myForm" action="index.php" method="POST">
+		<fieldset>
+			<legend>Upload file</legend>
+			Select file to upload: <input type="file" name="fileToUpload">
+			<br>
+			<input type="submit2" name ="submit" value="Upload File">
+		</fieldset>
+</form>
+</div>
+
+</body>
+
+</html> 
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Doc Ingestion - New</title>
+	<style>
+		div {
+			width: 40%;
+		}
+	</style>
+</head>
+
+<body>
+	<div>
+		<form action="ISBNresults.php" method="POST" name="form2">
+			<fieldset>
+				<legend>Get ISBN Information</legend>
+				Get info about title: <input type="text" name="isbn_input">
+				<input type="submit" name="submit" value="ISBN Info">
+			</fieldset>
+		</form>
+	</div>
+</body>
+
+</html> 
